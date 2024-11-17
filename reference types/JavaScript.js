6af14f238fcec663dbcf1e0dbb26595f90e7bbce
pranavkumar sakhare <pranavkumar6328@gmail.com>
2024-11-17 18:47:06 +0530

    hello css

---
+++ reference types/JavaScript.js
@@ -0,0 +1,110 @@
+// refrence types - objects , arrays , functions 
+// objects -
+// let course = {
+//     title:"HHLD" ,
+//     description:"projects" ,
+//     rating:5
+
+// } ;
+
+// console.log(course);
+// console.log(typeof(course));
+
+// // title notations
+
+// console.log(course.title);
+// console.log(course['title']);
+
+
+// let x = "pas" ;
+// let y = x ;
+
+// x = "hari";
+
+// console.log(x);
+// console.log(y);
+
+// let p = { name: " hari",
+//     age: 23 ,
+//     sername: " sakhare"
+// };
+
+// let q = p;
+  
+// p.name = " pas" ;
+
+// console.log(p.sername);
+// console.log(p);
+// console.log(q);
+// console.log(p.age);
+
+
+// array and functiions are objects
+
+// array
+
+// let course = [ 'hld','lld', 'dsa',6, true,null]
+
+// console.log(course[0]);
+// console.log(course[1]);
+// console.log(course[2]);
+// console.log(course[3]);
+// console.log(course[4]);
+// console.log(course[5]);
+
+// console.log(typeof(course));
+// console.log(course)
+
+
+// function cc(name)
+// {
+//     console.log('creating' + name );
+
+// }
+
+// cc(' hld');
+// cc(' lld'); 
+// //  functions
+
+// const , let , var
+
+
+
+
+
+// EXECUTION CONTEXT
+// 1. memory phase - variable envionment
+// 2. code phase - thread of exection
+
+// function cc(name)
+// {
+//     console.log('creating' + name );
+
+// }
+
+// cc(' hld');
+// cc(' lld'); 
+
+createcourse(' dsa')
+console.log(a);
+
+function createcourse(coursename)
+{
+console.log('create' + coursename );
+
+}
+
+var a = 10 ;
+console.log(a);
+createcourse('lld');
+
+console.log(a);
+console.log(this.a);
+console.log(window.a);
+
+
+console.log(window);
+console.log(this === window )
+
+
+
