669ee5b0d42705b602f27c0b2ccb58de42b7329b
pranavkumar sakhare <pranavkumar6328@gmail.com>
2024-11-17 09:57:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let x = "basketball"; 
+
+// js is dynamic language
+
+// string
+console.log(x);
+console.log(typeof(x)) ;
+
+// object
+x = null;
+console.log(typeof(x));
+
+
+// number (int , float)
+let y = 1.4 ;
+console.log(y);
+console.log(typeof(y));
+
+// boolean
+let p = true
+console.log(p);
+console.log(typeof(true))
+
+// undefined
+let w ;
+console.log(w);
+w=9;
+console.log(typeof(w))
+
+
+
